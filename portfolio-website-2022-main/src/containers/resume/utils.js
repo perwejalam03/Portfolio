7400002e6252985821cdeb5b3b1cdc52b13f72b9
perwejalam03 <perwejalam03@gmail.com>
2023-10-19 16:24:15 +0530

    first commit

---
+++ portfolio-website-2022-main/src/containers/resume/utils.js
@@ -0,0 +1,30 @@
+export const data = {
+    experience: [
+      {
+        title: "Backend Developer",
+        subTitle: "Bangalore, INDIA",
+        description:
+          "Developed E-Commerce website using spring boot and React JS, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "U M S Arihana Azamnagar Katihar",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "+2 AL H/S Sonaili Katihar",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Bengal College Of Engineering And Technology",
+        description:
+          "Bachelor of technology in electrical and electronics engineering",
+      },
+    ],
+  };
---
+++ portfolio-website-2022-main/src/containers/skills/utils.js
@@ -0,0 +1,79 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+        {
+          skillName: "React JS",
+          percentage: "80",
+        },
+        {
+          skillName: "BootStrap",
+          percentage: "70",
+        },
+        {
+          skillName: "Node JS",
+          percentage: "70",
+        },
+        {
+          skillName: "JSP",
+          percentage: "75",
+        },
+        
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "JAVA",
+          percentage: "80",
+        },
+        {
+          skillName: "J2EE",
+          percentage: "70",
+        },
+        {
+          skillName: "Hibernate",
+          percentage: "75",
+        },
+        {
+          skillName: "Spring Boot",
+          percentage: "80",
+        },
+        
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "65",
+        },
+        {
+          skillName: "ORACLE",
+          percentage: "85",
+        },
+        {
+          skillName: "mysql",
+          percentage: "85",
+        },
+      ],
+    },
+  ];
